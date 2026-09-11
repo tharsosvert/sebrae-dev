203219340ba2c760042ced261018ce6b60248cec
Sooza Digital <tharsosvert26@gmail.com>
2026-09-11 20:19:56 -0300

    files update

--- aula-js/sep-08.js
+++
@@ -1,6 +0,0 @@
-try{
-    let numero = Number(prompt("Digite um número"))
-    console.log("O número digitado foi " + numeros)
-} catch(errorVar) {
-    console.log("Algo deu errado: " + errorVar + " Tente novamente!")
-}
